e12785f1c5b9d319a5d3c4e55ed296809148e7c8
olumide1128 <59411943+olumide1128@users.noreply.github.com>
2020-09-16 13:09:06 +0100

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -2,7 +2,7 @@
 
 	// $conn = mysqli_connect("localhost","id13250376_root","=Tl5/Xlyd!u?rTz7","id13250376_quizappdb");
 
-	$conn = mysqli_connect("localhost","root","olumysql94","quizappdb");
+	$conn = mysqli_connect("localhost","<<username>>","<<dbPassword>>","<<dbName>>");
 
 	// Check connection
 	if (!$conn){
