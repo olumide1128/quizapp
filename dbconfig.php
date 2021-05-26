d211253aecc3c3d6b3dd2093735c5439b3eca13f
Olumide Oyegoke <59411943+olumide1128@users.noreply.github.com>
2021-05-26 14:39:51 +0100

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,5 @@
 <?php
 
-	// $conn = mysqli_connect("localhost","id13250376_root","=Tl5/Xlyd!u?rTz7","id13250376_quizappdb");
 
 	$conn = mysqli_connect("localhost","<<username>>","<<dbPassword>>","<<dbName>>");
 
